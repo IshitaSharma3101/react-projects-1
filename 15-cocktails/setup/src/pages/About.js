6843a2ff01fd4f29474bbaebdc06cd76d7d8f7c6
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-05-10 23:51:37 +0530

    cocktails project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut harum
+        exercitationem praesentium debitis est molestiae error facilis adipisci
+        porro distinctio. Optio cum maxime, quibusdam consequuntur amet, dicta,
+        numquam eos totam natus culpa libero fugiat quaerat voluptas? Officiis
+        tempora enim nisi.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
